1178f55692700090475ec9ebd63561cff53d1b90
Emerson Carvalho <8569519+brecarv@users.noreply.github.com>
2022-04-05 14:45:11 -0300

    container componentizado

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import { BrowserRouter as Router } from "react-router-dom";
 import Routes from "./routes";
+import { Container } from "@material-ui/core/";
 import Header from "./components/Header";
 
 const App = () => {
   return (
-    <Router>
-      <Header />
-      <Routes />
-    </Router>
+    <Container maxWith="xl">
+      <Router>
+        <Header />
+        <Routes />
+      </Router>
+    </Container>
   );
 };
 
