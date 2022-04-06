7bdf9d002ad38ce7c8f0909ffa32dd053a712cd9
Emerson Carvalho <8569519+brecarv@users.noreply.github.com>
2022-04-06 00:15:29 -0300

    redux estado global

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./components/store";
 import Routes from "./routes";
 import { Container } from "@material-ui/core/";
 import Header from "./components/Header";
 
 const App = () => {
   return (
-    <Container maxWith="xl">
-      <Router>
-        <Header />
-        <Routes />
-      </Router>
-    </Container>
+    <Provider store={store}>
+      <Container maxWith="xl">
+        <Router>
+          <Header />
+          <Routes />
+        </Router>
+      </Container>
+    </Provider>
   );
 };
 
