27f47e3cb93ff12d8ecfb2c5585f9666c46083fa
Emerson Carvalho <8569519+brecarv@users.noreply.github.com>
2022-07-06 13:56:29 -0300

    fix: correct classname

--- src/Pages/contato.js
+++ src/Pages/contato.js
@@ -1,18 +1,28 @@
 import { Button, Grid, TextField } from "@material-ui/core";
 import React from "react";
 
-const Contatos = () => {
+const Contato = () => {
   return (
     <>
       <Grid container direction="row" xs={12}>
-        <TextField id="name" label="Name" fullWidth />
-        <TextField id="message" label="Message" fullWidth />
+        <TextField id="name" label="Nome" fullWidth />
+        <TextField id="message" label="Mensagem" fullWidth />
       </Grid>
-      <Button variant="contained" color="primary">
+      <Button className="mt-2" variant="contained" color="primary">
         Sent
       </Button>
+
+      <div className="card mt-2">
+        <div className="card-body">
+          <h5 className="card-title">Autor</h5>
+          <p className="card-text"> Conteúdo da mensagem.</p>
+          <p className="card-text">
+            <small className="text-muted">Data da mensagem.</small>
+          </p>
+        </div>
+      </div>
     </>
   );
 };
 
-export default Contatos;
+export default Contato;
